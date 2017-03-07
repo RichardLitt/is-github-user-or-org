97b9ae0a02cb57109a932d8f7d50ee8368f323a8
Richard Littauer <richard.littauer@gmail.com>
2017-03-07 16:13:05 -0500

    Clean up logic, add token to call

--- index.js
+++ index.js
@@ -6,16 +6,15 @@ var octo
 
 module.exports = function (user, opts) {
   var opts = opts || {}
+  const token = opts.token || process.env.GITHUB_TOKEN
 
-  octo = new Octokat({
-    token: opts.token || process.env.GITHUB_TOKEN
-  })
+  octo = new Octokat({token})
 
   if (typeof user !== 'string') {
     throw new TypeError('Expected a string.')
   }
 
-  return getGithubUser(user).then((res) => {
+  return getGithubUser(user, {token}).then((res) => {
     if (res && res[0] && res[0].type) {
       return res[0].type
     } else {
