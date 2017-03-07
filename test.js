282bf2d209dda4ecf2b55b32d22cd467119c4877
Richard Littauer <richard.littauer@gmail.com>
2017-03-07 14:40:21 -0500

    Add token

--- test.js
+++ test.js
@@ -2,19 +2,25 @@ import test from 'ava'
 import fn from './'
 
 test(t => {
-  return fn('RichardLitt').then(result => {
+  const token = '523ef691191741c99d5afbcfe58079bfa0038771';
+  
+  return fn('RichardLitt', {token}).then(result => {
     t.is(result, 'User')
   })
 })
 
 test(t => {
-  return fn('OpenSourceDesign').then(result => {
+  const token = '523ef691191741c99d5afbcfe58079bfa0038771';
+
+  return fn('OpenSourceDesign', {token}).then(result => {
     t.same(result, 'Organization')
   })
 })
 
 test('throws', t => {
-    return fn('wkjhfw9a987sya323jkbsd7822dfsa').catch((error) => {
-      t.is(error.message, 'Not a GitHub user.');
-    })
+  const token = '523ef691191741c99d5afbcfe58079bfa0038771';
+
+  return fn('wkjhfw9a987sya323jkbsd7822dfsa', {token}).catch((error) => {
+    t.is(error.message, 'Not a GitHub user.');
+  })
 });
